1db6a0d240b4b02097657d8785bfb9f7ec8b4faf
Nishchit2 <67262453+Nishchit2@users.noreply.github.com>
2020-07-24 11:32:17 +0530

    Update GroundBaseClass.js

--- GroundBaseClass.js
+++ GroundBaseClass.js
@@ -6,7 +6,7 @@ class GroundBaseClass{
       this.body = Bodies.rectangle(x, y, width, height, options);
       this.width = width;
       this.height = height;
-      this.image = loadImage("sprites/base.png");
+      this.image = loadImage("base.png");
       World.add(world, this.body);
     }
     display(){
