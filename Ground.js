b4bf49314862a1756a21785b685d96e0e4ccb6b3
Nishchit2 <67262453+Nishchit2@users.noreply.github.com>
2020-07-24 11:31:49 +0530

    Update Ground.js

--- Ground.js
+++ Ground.js
@@ -1,6 +1,6 @@
 class Ground extends GroundBaseClass {
   constructor(x,y,width,height) {
     super(x,y,width,height);
-    this.image = loadImage("sprites/wood2.png");
+    this.image = loadImage("wood2.png");
   }
 }
